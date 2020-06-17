230b3dc1caa408b1500bf13437a95cd3099df70f
Sybille Hofer <kontakt@sybillehofer.ch>
2020-06-17 11:43:34 +0200

    remove category: from archive titles

--- assets/functions/hooks.php
+++ assets/functions/hooks.php
@@ -206,7 +206,24 @@ function cd_set_correct_taxonomies( $post_id, $post, $update ) {
     }
 	
 }
-
 add_action( 'save_post', 'cd_set_correct_taxonomies', 10,3 );
  
-
+/*
+ *
+ * remove "Category:" from archive titles
+ *
+*/
+add_filter( 'get_the_archive_title', function ($title) {    
+	if ( is_category() ) {    
+			$title = single_cat_title( '', false );    
+		} elseif ( is_tag() ) {    
+			$title = single_tag_title( '', false );    
+		} elseif ( is_author() ) {    
+			$title = '<span class="vcard">' . get_the_author() . '</span>' ;    
+		} elseif ( is_tax() ) { //for custom post types
+			$title = sprintf( __( '%1$s' ), single_term_title( '', false ) );
+		} elseif (is_post_type_archive()) {
+			$title = post_type_archive_title( '', false );
+		}
+	return $title;    
+});
