a3b8b7310275c22f420be0fbf7a6e740fa3c5ef5
Sybille Hofer <kontakt@sybillehofer.ch>
2021-03-24 11:39:48 +0100

    fix orbit for termIcons on filters

--- parts/proposal-filter-orbit.php
+++ parts/proposal-filter-orbit.php
@@ -11,6 +11,7 @@
                     <li class="orbit-slide">
                     <?php }
                         $index = 1;
+                        $length = count($items);
                         foreach( $items as $item ) : ?>
                             <span data-tooltip class="top filter-tooltip" title="<?php echo $item->name; ?>">
                                 <label title="<?php echo $item->name; ?>">
@@ -18,7 +19,7 @@
                                     <div class="termIcon"><img src="<?php echo dd_get_icon_url($item, ''); ?>" alt="<?php echo $item->name; ?>"></div>
                                 </label>
                             </span>
-                        <?php if( count($items) > 4 && $index % 4 == 0 ) { ?>
+                        <?php if( count($items) > 4 && $index % 4 == 0 && $index <= $length - 1) { ?>
                             </li><li class="orbit-slide">
                     <?php } 
                         $index++; endforeach;
